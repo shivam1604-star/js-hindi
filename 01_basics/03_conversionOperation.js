76e04d14c10bf278fb52caece16c4fbe8bf65638
shivam1604-star <shivamsingh16042004@gmail.com>
2026-07-07 11:37:00 +0000

    comparisionOperation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,65 @@
-let score = "undefined"
-console.log(score);
-console.log(typeof(score));
+// let score = "undefined"
+// console.log(score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(valueInNumber);
-console.log(typeof(valueInNumber));
-
-
-// conversions
-// "33" => 33
-// "33abc" => NaN    , but iska type toh number hai
-// true=1, false=0
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
 
 
-let isLoggedIn = 1
-let BooleanisLoggedIn = Boolean(isLoggedIn)
-console.log(BooleanisLoggedIn);
-console.log(typeof(BooleanisLoggedIn));
+// // conversions
+// // "33" => 33
+// // "33abc" => NaN    , but iska type toh number hai
+// // true=1, false=0
+
+
+// let isLoggedIn = 1
+// let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn);
+// console.log(typeof(BooleanisLoggedIn));
 
 
 
-// 1= true, 0=false, ""=false, "abc"=true, null=false, undefined=false
+// // 1= true, 0=false, ""=false, "abc"=true, null=false, undefined=false
  
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+
+
+
+
+// // ******************Operations******************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+// // console.log(2 + 3) // 5
+// // console.log("2" + "3") // 23
+// // console.log(2 + "3") // 23
+// // console.log("2" + 3) // 23
+// // console.log(2 + 3 + "4") // 54
+// // console.log("2" + 3 + 4) // 234
+// // console.log(2 + 3 + 4) // 9
+// // console.log(2 + 3 * 4) // 14
+
+
+// let str1 = "Hello "
+// let str2 = "World"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+ console.log(2 + true)  // output is 3, because true is converted to 1
+ console.log(2 + false) // output is 2, because false is converted to 0
+
+
+ let gameCounter = 100
+ gameCounter++ // gameCounter = gameCounter + 1
+ console.log(gameCounter);
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log(null > 0) // false
+console.log(null == 0) // false
+console.log(null >= 0) // true
+
+console.log(undefined > 0) // false
+console.log(undefined == 0) // false
+console.log(undefined >= 0) // false
+
+// === (strict equality - checks both value and type)
+console.log(null === 0) // false
+console.log(undefined === 0) // false
