414bebeb76ba3e34f6c2206d0b1c4ab60a9f850c
shivam1604-star <shivamsingh16042004@gmail.com>
2026-07-07 08:19:21 +0000

    container config done

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer version
+
+let age = 25
+let name = "John"
+let loggedIn = true
+
+console.log(age, name, loggedIn)
+console.log(typeof age, typeof name, typeof loggedIn)
+
+console.table([age, name, loggedIn])
+console.table([typeof age, typeof name, typeof loggedIn])
+
+// number => 2 ki power 52
+// string => any text, must be in either single or double quotes
+// boolean => true or false
+// null => standalone value
+// undefined => variable declared but no value assigned
+// symbol => unique identifier      
+
+
+// object
